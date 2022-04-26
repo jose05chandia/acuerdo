7894c740a10814b1dce4f855883376abbcb7954e
José <jose05chandia@gmail.com>
2022-04-25 22:05:50 -0400

    First commit, initial project

---
+++ src/main/java/com/acuerdo/acuerdo/AcuerdoApplication.java
@@ -0,0 +1,13 @@
+package com.acuerdo.acuerdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcuerdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcuerdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acuerdo/acuerdo/AcuerdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acuerdo.acuerdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcuerdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
